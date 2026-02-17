40a3611526388973b7f4f6e0fae645451ef4f08b
BenCookie95 <benkcooke@gmail.com>
2026-02-17 14:51:25 -0500

    remove unused errors

--- server/channels/app/post.go
+++ server/channels/app/post.go
@@ -429,16 +429,10 @@ func (a *App) CreatePost(rctx request.CTX, post *model.Post, channel *model.Chan
 			_, translateErr := a.AutoTranslation().Translate(rctx.Context(), model.TranslationObjectTypePost, rpost.Id, rpost.ChannelId, rpost.UserId, rpost)
 			if translateErr != nil {
 				var notAvailErr *model.ErrAutoTranslationNotAvailable
-				var systemPostErr *model.ErrAutoTranslationSystemPost
-				var userNotEnabledErr *model.ErrAutoTranslationUserNotEnabled
 				switch {
 				case errors.As(translateErr, &notAvailErr):
 					// Feature not available - log at debug level and continue
 					rctx.Logger().Debug("Auto-translation feature not available", mlog.String("post_id", rpost.Id), mlog.Err(translateErr))
-				case errors.As(translateErr, &systemPostErr):
-					// System post - not translatable, expected
-				case errors.As(translateErr, &userNotEnabledErr):
-					// User not opted in - translations still initialized for other users
 				case translateErr.Id == "ent.autotranslation.no_translatable_content":
 					// No translatable content (only URLs/mentions) - this is expected, don't log
 				default:
@@ -949,16 +943,10 @@ func (a *App) UpdatePost(rctx request.CTX, receivedUpdatedPost *model.Post, upda
 			_, translateErr := a.AutoTranslation().Translate(rctx.Context(), model.TranslationObjectTypePost, rpost.Id, rpost.ChannelId, rpost.UserId, rpost)
 			if translateErr != nil {
 				var notAvailErr *model.ErrAutoTranslationNotAvailable
-				var systemPostErr *model.ErrAutoTranslationSystemPost
-				var userNotEnabledErr *model.ErrAutoTranslationUserNotEnabled
 				switch {
 				case errors.As(translateErr, &notAvailErr):
 					// Feature not available - log at debug level and continue
 					rctx.Logger().Debug("Auto-translation feature not available for edited post", mlog.String("post_id", rpost.Id), mlog.Err(translateErr))
-				case errors.As(translateErr, &systemPostErr):
-					// System post - not translatable, expected
-				case errors.As(translateErr, &userNotEnabledErr):
-					// User not opted in - translations still initialized for other users
 				case translateErr.Id == "ent.autotranslation.no_translatable_content":
 					// No translatable content (only URLs/mentions) - this is expected, don't log
 				default:
